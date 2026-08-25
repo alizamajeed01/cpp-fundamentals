184a63069fe8bba1696b2308f28dfe3761228f12
AlizaMajeed01 <alizamajeed1030@gmail.com>
2026-08-25 11:15:22 +0500

    OOP Concept in cpp

---
+++ Classes-ObjectsInOOP.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+class Student
+{
+    public:
+    string name;
+    int age;
+    void display()
+    {
+        cout<<"Name: "<<name<<" , Age: "<<age<<endl;
+    }
+
+};
+int main()
+{
+    Student s1;
+
+    s1.name = "Aliza";
+    s1.age = 18;
+
+    s1.display();
+
+    return 0;
+}
