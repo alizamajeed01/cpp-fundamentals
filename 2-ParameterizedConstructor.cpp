daf1615b544e61353835480f4c33286fec8963bf
AlizaMajeed01 <alizamajeed1030@gmail.com>
2026-08-26 15:34:12 +0500

    Add second type of Constructors

---
+++ 2-ParameterizedConstructor.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Student
+{
+    int roll;
+
+    public:
+    Student(int r)
+    {
+        roll=r;
+        cout<<roll<<endl;
+    }
+};
+int main()
+{
+    Student s1(101),s2(102);
+
+    return 0;
+}
+    
