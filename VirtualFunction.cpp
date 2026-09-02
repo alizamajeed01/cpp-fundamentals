0c97cfb5aeea8717e38730f32743c82ff78838b9
AlizaMajeed01 <alizamajeed1030@gmail.com>
2026-09-02 19:40:59 +0500

    Add concept of virtual function and runtime polymorphism

---
+++ VirtualFunction.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Base
+{
+    public:
+      virtual void show(){
+        cout<<"Base class";
+      }
+};
+class Derived:public Base
+{
+    public:
+      void show(){
+        cout<<"Derived class";
+      }
+};
+int main()
+{
+    Base *ptr;
+    Derived d;
+    ptr=&d;
+    ptr->show();
+
+    return 0;
+}
